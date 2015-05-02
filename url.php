da7f869dfe164e79712c3544eab6a7a134291b14
Willian Leal <willian.leal@gmail.com>
2015-05-02 16:33:36 -0300

    Alteração do caminho dos arquivos

--- url.php
+++ url.php
@@ -1,7 +1,8 @@
 <?php
 	session_start();
 	
-	$dominio = "/Atividade aberta 05 UN2";
+	//$dominio = "/Atividade aberta 05 UN2";
 	
-	$url = 'http://'.$_SERVER['HTTP_HOST'].$dominio;
+	//$url = 'http://'.$_SERVER['HTTP_HOST'].$dominio;
+	$url = 'http://'.$_SERVER['HTTP_HOST'];
 ?>
